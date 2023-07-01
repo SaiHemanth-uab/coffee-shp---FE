b12a1dea6126c9f9ae5c68be532d82e15441b56c
Sai Hemanth Maremalla <saihemanthmaremalla@Sais-MBP.lan>
2023-07-01 18:01:36 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'online_market',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
