7893d469ea19a8725b7c76f1b6f0f37e1e9f568a
Sai Hemanth <smaremal@uab.edu>
2023-07-29 13:56:19 -0500

    added menu list from BE

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,37 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { MenuService } from "../services/menu.service";
-import { Router, NavigationExtras } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../services/menu.service';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
-  selector: "app-dashboard",
-  templateUrl: "./dashboard.page.html",
-  styleUrls: ["./dashboard.page.scss"],
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
   menuData: Array<any> = [];
-  constructor(public menuService: MenuService, public router: Router) {
-  }
+  constructor(public menuService: MenuService, public router: Router) {}
 
   ngOnInit() {
-    if(localStorage.getItem('newItem')){
-      this.menuData=JSON.parse(localStorage.getItem('newItem')??'');
-      localStorage.removeItem('newItem');
-      console.log(this.menuData,"local")
-    }
-    else{
-      this.getMenuData();
-    }
-     
-    
+    this.getMenuData();
   }
   getMenuData() {
-    this.menuService.getMenu().subscribe((data: any) => {
-      this.menuData = data;
-      console.log(this.menuData,",menu");
+    this.menuService.getMenuList().subscribe((data: any) => {
+      this.menuData = data.data;
+      console.log(this.menuData, ',menu');
     });
   }
   openCreateForm() {
-    this.router.navigate(["/dashboard/create"]);
+    this.router.navigate(['/dashboard/create']);
   }
   openCategory(categoryId: any, foodType: string) {
     this.router.navigate([`/dashboard/sub-category/${foodType}@${categoryId}`]);
